f35db5a8d4cb194d03b09a0eab9133fe8794f444
Suman <sumanj631@gmail.com>
2022-12-29 16:21:30 +0530

    .

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import linkRoutes from './backend/routes/link_route.js'
 import express from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
-import cors from 'cors'
+// import cors from 'cors'
 
 //dotenv config
 dotenv.config()
@@ -18,7 +18,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors())
+// app.use(cors())
 
 const __dirname = path.resolve();
 
